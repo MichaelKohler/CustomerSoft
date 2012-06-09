23ac45f9d57cff5eb73b86ceb9a8ca7e8fc5f1b7
Michael Kohler <michaelkohler@live.com>
2012-06-09 16:07:13 +0200

    License and Class doc added to StandardMenuBar

--- src/main/java/info/michaelkohler/customersoft/gui/StandardMenuBar.java
+++ src/main/java/info/michaelkohler/customersoft/gui/StandardMenuBar.java
@@ -1,5 +1,23 @@
 package info.michaelkohler.customersoft.gui;
 
+/*
+ * CustomerSoft
+ * Copyright (C) 2012  Michael Kohler <michaelkohler@linux.com>
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Event;
@@ -13,6 +31,13 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.KeyStroke;
 
+/**
+ * The standard menu bar which should be used by every window with a menu bar.
+ * 
+ * @author Michael Kohler <michaelkohler@linux.com>
+ * @version 0.0.1
+ *
+ */
 @SuppressWarnings("serial")
 public class StandardMenuBar extends JMenuBar {
     private static final int MENUITEM_WIDTH = 150;
